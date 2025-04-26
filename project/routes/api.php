67cc97a24617f45abf410838b360f4e21381eb34
ig0R <korkinigorttit@gmail.com>
2025-04-26 17:23:53 +0700

    Модель, контроллер и маршруты. Урок 3

--- project/routes/api.php
+++ project/routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware('api')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+    Route::prefix('v1')->group(function () {
+        Route::apiResource('/categories', \App\Http\Controllers\Api\V1\CategoryController::class);
     });
 });
